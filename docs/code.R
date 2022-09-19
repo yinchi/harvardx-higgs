c0c06539108883f4b7bb9387461bd98ebd08f9b6
Yin-Chi Chan <7994343+yinchi@users.noreply.github.com>
2022-09-19 06:48:03 +0100

    Remove lightgbm section, various other fixes

--- docs/code.R
+++ docs/code.R
@@ -1,4 +1,4 @@
-## ----Load-libraries--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+## ----Load-libraries-----------------------------------------------------------------------------------------------------------------------------------------------
 
 # R 4.1 key features: new pipe operator, \(x) as shortcut for function(x)
 # R 4.0 key features: stringsAsFactors = FALSE by default, raw character strings r"()"
@@ -7,8 +7,13 @@ if (packageVersion('base') < '4.1.0') {
 }
 
 if(!require("pacman")) install.packages("pacman")
+if(!require("keras")) {
+  install.packages("keras")
+  keras::install_keras()
+}
+
 library(pacman)
-p_load(data.table, dtplyr, tidyverse, R.utils, Rfast,
+p_load(data.table, dtplyr, tidyverse, R.utils, Rfast, knitr,
        lightgbm, keras, caret, pROC, knitr, conflicted)
 conflict_prefer('summarize', 'dplyr')
 conflict_prefer('summarise', 'dplyr')
@@ -16,14 +21,15 @@ conflict_prefer('filter', 'dplyr')
 conflict_prefer('between', 'dplyr')
 conflict_prefer('auc', 'pROC')
 
-if(!is_keras_available()) install_keras()
-
 # Somehow, this seems to prevent memory leaks
 tensorflow::tf$compat$v1$disable_eager_execution()
 
 
-## ----Download-data, results='hide'-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+## ----Download-data, results='hide'--------------------------------------------------------------------------------------------------------------------------------
 options(timeout=1800) # Give more time for the download to complete
+if(!dir.exists('data_raw')) {
+  dir.create('data_raw')
+}
 if(!file.exists('data_raw/HIGGS.csv')) {
   download.file(
     'https://archive.ics.uci.edu/ml/machine-learning-databases/00280/HIGGS.csv.gz',
@@ -31,7 +37,7 @@ if(!file.exists('data_raw/HIGGS.csv')) {
   gunzip('data_raw/HIGGS.csv.gz')
 }
 
-## ----Load-Data-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+## ----Load-Data----------------------------------------------------------------------------------------------------------------------------------------------------
 
 # Load dataset into memory
 higgs_all <- fread('data_raw/HIGGS.csv')
@@ -53,7 +59,7 @@ yAll <- higgs_all %>% select(signal) %>% as.data.table()
 rm(higgs_all)
 
 
-## ----Create-splits, results='hide'-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+## ----Create-splits, results='hide'--------------------------------------------------------------------------------------------------------------------------------
 
 # Create 10% test set split
 set.seed(1)
@@ -68,7 +74,7 @@ rm(xAll, yAll)
 gc()
 
 
-## ----Hist-momentum-features, fig.height=3.5, fig.width=7-------------------------------------------------------------------------------------------------------------------------------------------------------------
+## ----Hist-momentum-features, fig.height=3.5, fig.width=7----------------------------------------------------------------------------------------------------------
 
 x |>
   select(c(contains('_pT'), 'missing_E_mag')) |>
@@ -80,7 +86,7 @@ x |>
   ggtitle('Momentum features')
 
 
-## ----Hist-momentum-features-log, fig.height=3.5, fig.width=7---------------------------------------------------------------------------------------------------------------------------------------------------------
+## ----Hist-momentum-features-log, fig.height=3.5, fig.width=7------------------------------------------------------------------------------------------------------
 
 x |>
   select(c(contains('_pT'), 'missing_E_mag')) |>
@@ -93,7 +99,7 @@ x |>
   ggtitle('Momentum features (log transform)')
 
 
-## ----Skew-momentum-features------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+## ----Skew-momentum-features---------------------------------------------------------------------------------------------------------------------------------------
 
 tibble(
   Feature = x |>
@@ -115,7 +121,7 @@ tibble(
   kable(align = 'lrr', booktabs = T, linesep = '')
 
 
-## ----Hist-angular-features, fig.height=5, fig.width=6----------------------------------------------------------------------------------------------------------------------------------------------------------------
+## ----Hist-angular-features, fig.height=5, fig.width=6-------------------------------------------------------------------------------------------------------------
 
 x |>
   select(c(contains('_eta'), contains('_phi'))) |>
@@ -128,7 +134,7 @@ x |>
   ggtitle('Angular features')
 
 
-## ----Hist-btags, fig.height=3.5, fig.width=4-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+## ----Hist-btags, fig.height=3.5, fig.width=4----------------------------------------------------------------------------------------------------------------------
 
 x |>
   select(contains('_btag')) |>
@@ -140,7 +146,7 @@ x |>
   ggtitle('b-tag features')
 
 
-## ----btag-means------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+## ----btag-means---------------------------------------------------------------------------------------------------------------------------------------------------
 
 # b-tag means
 tibble(
@@ -159,7 +165,7 @@ Mean = x |>
   kable(align = 'lrr', booktabs = T, linesep = '')
 
 
-## ----Hist-high-level-features, fig.height=5, fig.width=7-------------------------------------------------------------------------------------------------------------------------------------------------------------
+## ----Hist-high-level-features, fig.height=5, fig.width=7----------------------------------------------------------------------------------------------------------
 
 x |>
   select(contains('m_')) |>
@@ -171,7 +177,7 @@ x |>
   ggtitle('High-level features')
 
 
-## ----Hist-high-level-features-log, fig.height=5, fig.width=7---------------------------------------------------------------------------------------------------------------------------------------------------------
+## ----Hist-high-level-features-log, fig.height=5, fig.width=7------------------------------------------------------------------------------------------------------
 
 x |>
   select(contains('m_')) |>
@@ -184,7 +190,7 @@ x |>
   ggtitle('High-level features (log transform)')
 
 
-## ----Skew-high-level-features----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+## ----Skew-high-level-features-------------------------------------------------------------------------------------------------------------------------------------
 
 tibble(
   Feature = x |>
@@ -206,7 +212,7 @@ tibble(
   kable(align = 'lrr', booktabs = T, linesep = '')
 
 
-## ----Log-transform, results='hide'-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+## ----Log-transform, results='hide'--------------------------------------------------------------------------------------------------------------------------------
 
 # Apply log transform
 x <- x |>
@@ -220,124 +226,19 @@ x <- x %>% as.matrix()
 gc()
 
 
-## ----Mean-SD-scaling-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+## ----Mean-SD-scaling----------------------------------------------------------------------------------------------------------------------------------------------
 
 m <- colMeans(x)
 sd <- colVars(x, std = T) # std=T -> compute st. dev. instead of variance
 x <- scale(x, center = m, scale = sd)
 
 
-## ----Extract-signal--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+## ----Extract-signal-----------------------------------------------------------------------------------------------------------------------------------------------
 
 y <- y$signal
 
 
-## ----lgb-drop-rate, results='hide'-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-# Drop rates
-dr <- c(0, 0.01, 0.02, seq(0.025, 0.07, 0.005), 0.08, 0.09, 0.1, 0.15, 0.2, 0.3, 0.5)
-
-# Although we are not using tensorflow here, this is a convenience function
-# to set multiple seeds across R and Python (i.e. reticulate)
-tensorflow::set_random_seed(42, disable_gpu = F)
-
-if (!file.exists('cache/gbm_results.RData')) {
-  # Train
-  gbm_scores <- sapply(dr, function(d) {
-    gc()
-    params <- list(
-      num_threads = 32, # hardware-dependent
-      boosting = "dart",
-      metric = "auc",
-      learning_rate = 1.0, # found to be a good value, too large leads to instability
-      seed = 42, # note, any other seeds with default values take priority
-      
-      drop_rate = d
-    )
-    gbmodel <- lgb.cv(
-      params, x, label = y,
-      nrounds = 100, nfold = 3, obj = 'binary', verbose = 0
-    )
-    gbmodel$best_score
-  })
-  
-  # Save
-  save(gbm_scores, file = 'cache/gbm_results.RData')
-}
-
-load('cache/gbm_results.RData')
-tensorflow::set_random_seed(42, disable_gpu = F)
-
-
-## ----lightgbm-AUC-plot, fig.height=3, fig.width=4--------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-tibble(dr,gbm_scores) |>
-  ggplot(aes(dr,gbm_scores)) +
-  geom_line() +
-  geom_point() +
-  xlab('Drop rate') +
-  ylab('AUC') +
-  theme_gray()
-
-
-## ----lightgbm-best-AUC-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-cat('The best AUC of', max(gbm_scores),
-    'is achieved with a drop rate of', dr[which.max(gbm_scores)], '.\n')
-
-
-## ----lightgbm-min-AUC-in-range---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-  
-min(gbm_scores[between(dr,0.04,0.065)])
-
-
-## ----lightgbm-best-model, results='hide'-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-tensorflow::set_random_seed(42, disable_gpu = F)
-if (!file.exists('cache/gbm_results_2.RData')) {
-  
-  # Get the best drop rate from before
-  best_dr <- dr[which.max(gbm_scores)]
-  
-  # Train the model again
-  params <- list(
-    num_threads = 32, # hardware-dependent
-    boosting = "dart",
-    metric = "auc",
-    learning_rate = 1.0, # found to be a good value, too large leads to instability
-    seed = 42, # note, any other seeds with default values take priority
-    
-    drop_rate = best_dr
-  )
-  gbmodel <- lgb.train(
-    params, lgb.Dataset(x, label = y),
-    nrounds = 100, obj = 'binary', verbose = 0
-  )
-  
-  # Compute AUC
-  auc_lgb_best <- auc(y,predict(gbmodel,x)) |> as.numeric()
-  
-  # Save AUC and model
-  save(auc_lgb_best, gbmodel, file='cache/gbm_results_2.RData')
-  cat(lgb.dump(gbmodel), file='cache/lgb_model.json') # (almost) human-readable version
-}
-
-load('cache/gbm_results_2.RData')
-lgb.restore_handle(gbmodel) # if model is needed, we need to fully restore it
-tensorflow::set_random_seed(42, disable_gpu = F)
-
-
-## ----lightgbm-best-model-AUC-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-auc_lgb_best
-
-
-## ----lgb-importance, fig.height=4, fig.width=4-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-par(cex=0.7)
-gbmodel |> lgb.importance() |> lgb.plot.importance(top_n = 10)
-
-
-## ----Keras-history---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+## ----Keras-history------------------------------------------------------------------------------------------------------------------------------------------------
 
 train_keras_history <- function(x, y, depth, breadth,
                         dropout = 0.5, learning_rate=0.0002, epochs = 50) {
@@ -377,7 +278,7 @@ train_keras_history <- function(x, y, depth, breadth,
 }
 
 
-## ----Keras-learning-rate-effect, results='hide'----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+## ----Keras-learning-rate-effect, results='hide'-------------------------------------------------------------------------------------------------------------------
 tensorflow::set_random_seed(42, disable_gpu = F)
 if (!file.exists('cache/nn_results.RDdata')) {
   history1 <- train_keras_history(x, y, 3, 256, learning_rate = 1e-3)
@@ -389,7 +290,7 @@ load('cache/nn_results.RDdata')
 tensorflow::set_random_seed(42, disable_gpu = F)
 
 
-## ----fig.height=3, fig.width=5---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+## ----fig.height=3, fig.width=5------------------------------------------------------------------------------------------------------------------------------------
 tibble(
   epoch = seq(50),
   `1e-3` = history1$metrics$val_loss,
@@ -404,7 +305,7 @@ tibble(
   labs(color='Learning Rate')
 
 
-## ----Keras-tuning-function-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+## ----Keras-tuning-function----------------------------------------------------------------------------------------------------------------------------------------
 
 # Tuning values given defaults are not included in our parameter search for this
 # project.
@@ -467,7 +368,7 @@ train_keras_auc <- function(x, y,
 }
 
 
-## ----Keras-tuning-run, results='hide'--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+## ----Keras-tuning-run, results='hide'-----------------------------------------------------------------------------------------------------------------------------
 
 # Try NN training for different NN depths and breadths.  Cache using .RData file.
 tensorflow::set_random_seed(42, disable_gpu = F)
@@ -488,7 +389,7 @@ load('cache/nn_results2.RDdata')
 tensorflow::set_random_seed(42, disable_gpu = F)
 
 
-## ----Keras-tuning-plot, fig.height=3.5, fig.width=4.5----------------------------------------------------------------------------------------------------------------------------------------------------------------
+## ----Keras-tuning-plot, fig.height=3.5, fig.width=4.5-------------------------------------------------------------------------------------------------------------
 
 # heatmap of AUC vs depth and breadth
 nn_results |> ggplot(aes(as.factor(depth), as.factor(breadth), fill = auc)) +
@@ -499,56 +400,23 @@ nn_results |> ggplot(aes(as.factor(depth), as.factor(breadth), fill = auc)) +
   ylab('Breadth (hidden nodes per layer)')
 
 
-## ----train-keras, eval=FALSE, include=FALSE--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-## 
-## train_keras <- function(x, y,
-##                         depth, breadth,
-##                         dropout = 0.5, learning_rate = 0.0002,
-##                         epochs = 50) {
-## 
-##   model <- keras_model_sequential()
-## 
-##   # first hidden layer
-##   model |>
-##     layer_dense(units = breadth,
-##                 activation = 'relu',
-##                 input_shape = ncol(x)) |>
-##     layer_dropout(rate = dropout)
-## 
-##   # subsequent hidden layers
-##   if (depth > 1) {
-##     for (layer in seq(2,depth)) {
-##       model |>
-##         layer_dense(units = breadth, activation = 'relu') |>
-##         layer_dropout(rate = dropout)
-##     }
-##   }
-## 
-##   # output layer (logistic activation function for binary classification)
-##   model |>
-##     layer_dense(units = 1, activation = 'sigmoid')
-## 
-##   # compile model
-##   model |>
-##     keras::compile(
-##       loss = 'binary_crossentropy',
-##       optimizer = optimizer_adam(learning_rate = learning_rate),
-##       metrics = metric_auc()
-##     )
-## 
-##   # a larger batch size trains faster but uses more GPU memory
-##   model |>
-##     fit(x, y,
-##         epochs = epochs, batch_size = 8192,
-##         validation_split = 0.2)
-## }
-## 
-## tensorflow::set_random_seed(42, disable_gpu = F)
-## if (!file.exists('cache/nn_results3.RDdata')) {
-##   model_full <- train_keras(x, y, 3, 2048, learning_rate = 2e-4)
-##   model_low_only <- train_keras(x[,1:21], y, 3, 2048, learning_rate = 2e-4)
-##   save(history4, history5, file='cache/nn_results3.RDdata')
-## }
-## load('cache/nn_results3.RDdata')
-## tensorflow::set_random_seed(42, disable_gpu = F)
+## ----train-keras--------------------------------------------------------------------------------------------------------------------------------------------------
+
+tensorflow::set_random_seed(42, disable_gpu = F)
+if (!file.exists('cache/nn_results3.RDdata')) {
+  auc_low_only <- train_keras_auc(x[,1:21], y, 3, 2048, learning_rate = 2e-4)
+  save(auc_low_only, file='cache/nn_results3.RDdata')
+}
+load('cache/nn_results3.RDdata')
+tensorflow::set_random_seed(42, disable_gpu = F)
+
+
+## ----auc-all-vs-low-----------------------------------------------------------------------------------------------------------------------------------------------
+tibble(
+  Features = c('All', 'Low-level only'),
+  AUC = c(
+    nn_results |> filter(breadth == 2048 & depth == 3) |> pull(auc),
+    auc_low_only
+  )
+) |> kable(align = 'lr', booktabs = T, linesep = '')
 
